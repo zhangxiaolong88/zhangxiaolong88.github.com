50a39ff252d7a1c3ea3ebdf5cd37aeb995956a59
zhangxiaolong <xlzhang1988@gmail.com>
2014-04-15 22:54:59 +0800

    javascript effect

---
+++ _posts/2014-04-14-series-of-javascript-effect.md
@@ -0,0 +1,27 @@
+---
+layout: effect
+title: "每周一特效 之 一"
+keywords: JavaScript effect
+description: "I wish I would update this series everyweek."
+category: javascript
+tags: [javascript, effect]
+---
+{% include JB/setup %}
+
+<!-- more -->
+
+<ul class="border">
+	<li><img src="/assets/images/javascript-effect/IMG_0161.JPG" width="150" height="280" /></li>
+	<li><img src="/assets/images/javascript-effect/IMG_0201.JPG" width="150" height="280" /></li>
+	<li><img src="/assets/images/javascript-effect/IMG_0263.JPG" width="150" height="280" /></li>
+	<li><img src="/assets/images/javascript-effect/IMG_0272.JPG" width="150" height="280" /></li>
+	<li><img src="/assets/images/javascript-effect/IMG_0314.JPG" width="150" height="280" /></li>
+</ul>
+
+```
+$("img").border({
+	speed: "slow",
+	color: "pink",
+	width: 5
+});
+```
---
+++ assets/themes/the-program/js/jquery.border.js
@@ -0,0 +1,112 @@
+﻿/**
+ * jquery.border.js v0.1
+ * http://blog.zhangxiaolong.me
+ *
+ * Copyright 2014, zhangxiaolong
+ * http://blog.zhangxiaolong.me
+ */
+;
+(function($, window, undefined) {
+	'use strict';
+
+	$.Border = function(options, element) {
+		this.$el = element;
+		this.init(options);
+	};
+
+	$.Border.defaults = {
+		speed: "slow",
+		width: 3,
+		color: "#000"
+	};
+
+	$.Border.prototype.init = function(opts) {
+		var self = this;
+		//init options
+		self.options = $.extend({}, $.Border.defaults, opts);
+
+		self.$el.each(function(i, v){
+			var $img = $(v);
+			var boxer = "<div class='border_boxer'>" 
+				+ "<div class='top_border'></div>" 
+				+ "<div class='left_border'></div>" 
+				+ "<div class='bottom_border'></div>" 
+				+ "<div class='right_border'></div>"
+			+ "</div>";
+			$img.parent().append($(boxer));
+			var $borderBoxer = $(v).parent().find(".border_boxer");
+			$borderBoxer.append($img);
+			$borderBoxer.css({
+				"position": "relative",
+				"padding": self.options.width,
+				"margin": 0,
+				"height": $img.height(),
+				"width": $img.width()
+			});
+
+			(function($img, $boxer){
+				$img.unbind("mouseover").mouseover(function(evt){
+					console.log("mouseover");
+					$boxer.find(".top_border").css({
+						"position": "absolute",
+						"top": 0,
+						"left": 0,
+						"width": "1%", //$(v).width() + self.options.width * 2,
+						"height": self.options.width,
+						"background-color": self.options.color
+					});
+					$boxer.find(".left_border").css({
+						"position": "absolute",
+						"bottom": 0,
+						"left": 0,
+						"height": "1%",
+						"width": self.options.width,
+						"background-color": self.options.color
+					});
+					$boxer.find(".bottom_border").css({
+						"position": "absolute",
+						"bottom": 0,
+						"right": 0,
+						"width": "1%", 
+						"height": self.options.width,
+						"background-color": self.options.color
+					});
+					$boxer.find(".right_border").css({
+						"position": "absolute",
+						"top": 0,
+						"right": 0,
+						"height": "1%",
+						"width": self.options.width,
+						"background-color": self.options.color
+					}).delay(100).animate({
+						"height": "100%"
+					},{
+						duration: self.options.speed,
+						easing: "swing",
+						step: function(per, obj){
+							$boxer.find(".top_border").css({"width": per + "%"});
+							$boxer.find(".left_border").css({"height": per + "%"});
+							$boxer.find(".bottom_border").css({"width": per + "%"});
+						}
+
+					});
+					evt.stopPropagation();
+				}).unbind("mouseout").mouseout(function(evt){
+					$boxer.find(".top_border, .bottom_border").stop(true).css({
+						"width": 0
+					});
+					$boxer.find(".left_border, .right_border").stop(true).css({
+						"height": 0
+					});
+					evt.stopPropagation();
+					return false;
+				});
+			})($img, $borderBoxer);
+		});
+	};
+
+	$.fn.border = function(options) {
+		return new $.Border(options, this);
+	}
+
+})(jQuery, window);
